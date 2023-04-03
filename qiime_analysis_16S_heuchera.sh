3aba423c17b2755573fa367af3c00f8d2e6a6321
Ryan A. Folk <folk.41@buckeyemail.osu.edu>
2023-04-03 16:15:27 -0500

    Update qiime_analysis_16S_heuchera.sh

--- qiime_analysis_16S_heuchera.sh
+++ qiime_analysis_16S_heuchera.sh
@@ -125,7 +125,7 @@ qiime taxa barplot --i-table table.qza --i-taxonomy taxonomy.withhost.qza --m-me
 #######
 # Aggregate taxon barplots to host species
 
-qiime feature-table group --i-table table.hostremoved.qza --m-metadata-file sample-metadata.tsv --m-metadata-column host_species --p-axis sample --p-mode mean-ceiling --o-grouped-table table.grouped_hostspecies # note lack of extension
+qiime feature-table group --i-table table.organellesremoved.qza --m-metadata-file sample-metadata.tsv --m-metadata-column host_species --p-axis sample --p-mode mean-ceiling --o-grouped-table table.grouped_hostspecies # note lack of extension
 qiime taxa barplot --i-table table.grouped_hostspecies.qza --i-taxonomy taxonomy.qza --m-metadata-file sample-metadata.grouped_hostspecies.tsv --o-visualization taxa-bar-plots.grouped_hostspecies.qzv
 
 
